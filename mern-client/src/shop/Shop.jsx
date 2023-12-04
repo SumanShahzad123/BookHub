7a8c1a9a914061b7929fdf7574db537d6ec65e3e
husnainmazhar45 <144983962+husnainmazhar45@users.noreply.github.com>
2023-12-04 22:43:39 +0500

    Update shop

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -10,7 +10,7 @@ const Shop = () => {
     <div className='mt-28 px-4 lg:px-24'>
       <h2 className='text-5xl font-bold text-center'>All Books are here</h2>
       <div>
-
+           {/*not completed yet*/}
       </div>
     </div>
   )
