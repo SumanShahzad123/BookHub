f8e177ca0563a105bef645967f77f5f15f5bbf39
husnainmazhar45 <144983962+husnainmazhar45@users.noreply.github.com>
2023-12-05 23:35:18 +0500

    Update dasboard

--- mern-client/src/dashboard/Dashboard.jsx
+++ mern-client/src/dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <div className='mt-20 px-4 lg:px-24'>
     <h2 className='text-5xl font-bold '>Mumtaz Book Store</h2>
     <div>
-
+          {/*incomplete*/}
     </div>
   </div>
   )
